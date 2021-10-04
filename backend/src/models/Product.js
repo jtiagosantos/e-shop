d40f4b489d3e5f01dc187c4448b929ea23d6db17
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-10-03 23:11:42 -0500

    feat: add products modal

---
+++ backend/src/models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  inventory: {
+    type: String,
+    required: true
+  },
+  created_at: { 
+    type: Date, 
+    default: new Date()
+  },
+  updated_at: { 
+    type: Date, 
+    default: new Date()
+  }
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product;
