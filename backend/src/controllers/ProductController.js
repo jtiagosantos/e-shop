b923771e378e0768aaa10581accb0d69ea7e7256
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-10-05 17:08:05 -0500

    fix/feat: fix bugs and add features to the controller

--- backend/src/controllers/ProductController.js
+++ backend/src/controllers/ProductController.js
@@ -1,11 +1,11 @@
-const Produtc = require('../models/Product');
+const Product = require('../models/Product');
 
 class ProductController {
   async createProduct(req, res) {
     const body = req.body;
 
     try {
-      const product = await new Produtc(body).save();
+      const product = await new Product(body).save();
       res.status(200).json({ error: false, product });
     } catch(error) {
       res.status(500).json({ error: error.message });
@@ -14,7 +14,7 @@ class ProductController {
 
   async readProducts(req, res) {
     try {
-      const products = await Produtc.find();
+      const products = await Product.find();
       res.status(200).json({ products });
     } catch(error) {
       res.status(500).json({ error: error.message });
@@ -25,8 +25,9 @@ class ProductController {
     const { id } = req.params;
 
     try {
-      await Produtc.findByIdAndDelete(id);
-      res.status(200).json({ error: false, message: 'Product deleted!' });
+      const product = await Product.findByIdAndDelete(id);
+      if(product) res.status(200).json({ error: false, message: 'Product deleted!' });
+      else res.status(404).json({ error: false, message: 'Product not found!' });
     } catch(error) {
       res.status(500).json({ error: error.message });
     }
@@ -35,10 +36,12 @@ class ProductController {
   async updateProduct(req, res) {
     const { id } = req.params;
     const body = req.body;
+    
+    body.updated_at = new Date();
 
     try {
-      const productUpdated = await Produtc.findByIdAndUpdate(id, body, { new: true });
-      res.status(200).json({ error: false, message: 'Product updated!', productUpdated });
+      const productUpdated = await Product.findByIdAndUpdate(id, body, { new: true });
+      res.status(200).json({ error: false, message: 'Product updated!', product: productUpdated });
     } catch(error) {
       res.status(500).json({ error: error.message });
     }
