72d84cf512652fd6f627071966143463d40028bc
jtiagosantos <tiago.santos@icomp.ufam.edu.br>
2021-10-11 23:14:06 -0500

    feat: add caching for files

--- backend/src/controllers/FileController.js
+++ backend/src/controllers/FileController.js
@@ -1,3 +1,4 @@
+const { syncRedisGet, syncRedisSet } = require("../../redis/config");
 const File = require('../models/File');
 
 class FileController {
@@ -11,6 +12,8 @@ class FileController {
         filename: file.originalname,
       }).save();
 
+      await syncRedisSet('files', '');
+
       res.status(200).json({ imageFile });
     } catch(error) {
       res.status(500).json({ error: error.message });
@@ -19,8 +22,15 @@ class FileController {
 
   async index(req, res) {
     try {
-      const files = await File.find();
-      res.status(200).json({ files });
+      const files = await syncRedisGet('files');
+
+      if(!files) {
+        const files = await File.find();
+        await syncRedisSet('files', JSON.stringify(files));
+        res.status(200).json({ files });
+      }
+
+      res.status(200).json({ files: JSON.parse(files) });
     } catch(error) {
       res.status(500).json({ error: error.message });
     }
